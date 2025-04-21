a9d23937f0bf6212192c3b7afd954912bce9d60e
rybhrdwj <25ricky08@gmail.com>
2025-04-21 21:51:12 +0530

    implement formula parser

--- client/services/formulaParser.js
+++ client/services/formulaParser.js
@@ -13,52 +13,39 @@ import DependencyGraph from "./dependencyGraph.js";
 */
 
 class FormulaParser {
-  constructor(getCellByCoordinate, setCellByCoordinate, dependencyGraph = null) {
+  constructor(expression = null) {
+    this.expression = expression;
     this.parser = new Parser();
-this.dependencyGraph = dependencyGraph || new DependencyGraph(); // Use injected instance or create a new one
-    this.formulaCache = new Map();
-    this.expression = null;
-
-    this.getCellByCoordinate = getCellByCoordinate; // Injected function to fetch cell values
-    this.setCellByCoordinate = setCellByCoordinate; // Injected function to set cell values
+    this.formula = expression ? this.parser.parse(expression) : null;
   }
 
-  setFormula(cell, formula) {
-    if (this.formulaCache.has(cell)) {
-      const oldFormula = this.formulaCache.get(cell);
-
-      if (oldFormula === formula) {
-        return;
-      }
-    }
-
-    this.formulaCache.set(cell, formula);
-
-    this.expression = this.parser.parse(formula);
-
-    const dependencies = this.expression.variables();
-
-    // TODO: Update the table to use getDependencies to fetch them,
-    // and call evaluateExpression with cellValues to get value
+  getExpression() {
+    return this.expression;
   }
 
-  getDependencies() {
-    return this.expression.variables();
+  setExpression(expression) {
+    this.expression = expression;
+    this.formula = this.parser.parse(expression);
   }
 
+  /*
+   * Returns the symbols in the expression. This is used to get the cell references in the formula.
+   * @returns {Array} - An array of symbols in the expression.
+   */
+  getVariables() {
+    return this.formula.variables();
+  }
+
+  /*
+   * Evaluates the expression with the given cell values.
+   * @param {Object} cellValues - An object containing cell values.
+   * @returns {number | null}
+   */
   evaluateExpression(cellValues) {
     if (!this.expression) return null;
 
-    return this.expression.evaluate(cellValues);
+    return this.formula.evaluate(cellValues);
   }
 }
 
 export default FormulaParser;
-
-// const parser = new Parser();
-// let parsed = parser.parse("A1 + B1 * 2 - C3");
-// console.log(parsed.symbols());
-// console.log(parsed.evaluate({ A1: 10, C3: 5, B1: 2 }));
-// parsed = parser.parse("A1 + B1 * 2");
-// console.log(parsed.symbols());
-// console.log(parsed.evaluate({ A1: 10, C3: 5, B1: 2 }));
