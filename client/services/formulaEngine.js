a731adb78370fcae303401b2e8a4bbb2382c7b00
rybhrdwj <25ricky08@gmail.com>
2025-04-22 16:53:44 +0530

    fix critical bug in algorithm which caused wrong values to be set

--- client/services/formulaEngine.js
+++ client/services/formulaEngine.js
@@ -13,8 +13,11 @@ class FormulaEngineMediator {
     this.table = table;
   }
 
-  updateFormulaAndDependents(cell, formula) {
+    updateFormulaAndDependents(cell, formula) {
 
+    if (formula[0] === "=") {
+      formula = formula.slice(1);
+    }
     this.formulaParser.setExpression(formula);
     const dependencies = this.formulaParser.getVariables();
 
@@ -38,25 +41,34 @@ class FormulaEngineMediator {
         this.formulaParser.setExpression(cellFormulas[coordinate]);
 
         // FIXME: #ERROR if formula invalid OR variable not found / invalid
-        
         const requiredCellValues = this.formulaParser.getVariables();
         const requiredValues = this.table.getCellsByCoordinate(requiredCellValues, { returnType: "value" });
         const evaluatedValue = this.formulaParser.evaluateExpression(requiredValues);
         
-        this.table.setCellByCoordinate(cell, evaluatedValue);
+        this.table.setCellByCoordinate(coordinate, evaluatedValue);
     });
   }
 }
 
-export default FormulaEngineMediator;
+export default new FormulaEngineMediator(new Table(), new DependencyGraph());
 
 // TESTING PURPOSE ONLY
-const formulaEngine = new FormulaEngineMediator(new Table(), new DependencyGraph());
-formulaEngine.table.setCellByCoordinate("B1", 5);
-formulaEngine.table.setCellByCoordinate("C1", 5);
-formulaEngine.table.setCellByCoordinate("A1", 0, "B1 + C1");
-formulaEngine.updateFormulaAndDependents("A1", "B1 + C1");
-console.log(formulaEngine.dependencyGraph.getDependencies("A1"));
-console.log(formulaEngine.table.getCellByCoordinate("B1")); // Should print 5
-console.log(formulaEngine.table.getCellsByCoordinate(["A1", "B1", "C1"], { returnType: "value" })); // Should print the values of A1, B1, and C1
-console.log(formulaEngine.table.getCellByCoordinate("A1")); // Should print the evaluated value of A1 based on B1 and C1
+// const formulaEngine = new FormulaEngineMediator(new Table(), new DependencyGraph());
+// formulaEngine.table.setCellByCoordinate("B1", '6');
+// formulaEngine.table.setCellByCoordinate("C1", '5');
+
+// formulaEngine.table.setCellByCoordinate("A1", "ab", "=B1 + C1");
+// formulaEngine.updateFormulaAndDependents("A1", "B1 + C1");
+// console.log(formulaEngine.table.getCellsByCoordinate(["A1", "B1", "C1", "D2"], { returnType: "cell" })); // Should print the formulas of A1, B1, and C1
+
+// formulaEngine.table.setCellByCoordinate("B1", "" , "=C1 + 2");
+// formulaEngine.updateFormulaAndDependents("B1", "C1 + 2");
+
+// formulaEngine.table.setCellByCoordinate("D2", "", "=B1 + 2");
+// formulaEngine.updateFormulaAndDependents("D2", "B1 + 2");
+// console.log(formulaEngine.table.getCellsByCoordinate(["A1", "B1", "C1", "D2"], { returnType: "cell" })); // Should print the formulas of A1, B1, and C1
+
+// console.log(formulaEngine.dependencyGraph.getDependencies("A1"));
+// console.log(formulaEngine.table.getCellByCoordinate("B1")); // Should print 5
+// console.log(formulaEngine.table.getCellsByCoordinate(["A1", "B1", "C1"], { returnType: "value" })); // Should print the values of A1, B1, and C1
+// console.log(formulaEngine.table.getCellByCoordinate("A1")); // Should print the evaluated value of A1 based on B1 and C1
