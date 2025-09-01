1d599474cbeecc5d75a9c51b37849cebe2337679
rybhrdwj <25ricky08@gmail.com>
2025-09-01 18:16:21 +0530

    overhaul landing page

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,6 +1,5 @@
 import Hero from './Hero';
 import Features from './Features';
-import Demo from './Demo';
 import CTA from './CTA';
 
 function Landing() {
@@ -8,7 +7,6 @@ function Landing() {
     <div className="min-h-screen bg-gray-900">
       <Hero />
       <Features />
-      <Demo />
       <CTA />
     </div>
   );
